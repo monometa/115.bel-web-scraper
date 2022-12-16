d52de7a1acf7a24a30331495034d276ea26607ce
monometa <il.kaltovich@gmail.com>
2022-12-16 03:55:21 +0300

    fix: remove AWS provider hadoop param

--- data_processing_spark.py
+++ data_processing_spark.py
@@ -44,10 +44,10 @@ def get_spark():
     spark._jsc.hadoopConfiguration().set(
         "fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem"
     )
-    spark._jsc.hadoopConfiguration().set(
-        "fs.s3a.aws.credentials.provider",
-        "com.amazonaws.auth.InstanceProfileCredentialsProvider,com.amazonaws.auth.DefaultAWSCredentialsProviderChain",
-    )
+    # spark._jsc.hadoopConfiguration().set(
+    #     "fs.s3a.aws.credentials.provider",
+    #     "com.amazonaws.auth.InstanceProfileCredentialsProvider,com.amazonaws.auth.DefaultAWSCredentialsProviderChain",
+    # )
     spark._jsc.hadoopConfiguration().set(
         "fs.AbstractFileSystem.s3a.impl", "org.apache.hadoop.fs.s3a.S3A"
     )
